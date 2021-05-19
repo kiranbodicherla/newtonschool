0ac294ba7bb8a0e270205d6708553c8b1fcbd926
kiranbodicherla <31443578+kiranbodicherla@users.noreply.github.com>
2021-05-19 20:47:56 +0530

    Update and rename firstassign to firstassign.js

---
+++ firstassign.js
@@ -0,0 +1,25 @@
+import React, (Component, useState) from "react"
+
+import ../styles/App.css";
+
+const App = () =>
+
+return (
+
+{
+
+<div id="main">
+
+<div id="root">
+
+<p>I am learning React. My life is getti
+
+</div>
+
+</div>
+
+)
+
+}
+
+export default App:
